5da089e0bc3272278f4162ec06c74ee0cd1ed94c
Connor Paull <connor.paull@gmail.com>
2016-08-25 12:53:38 -0700

    more glitch fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :secrets
   has_many :likes, dependent: :destroy
   has_many :secrets_liked, through: :likes, source: :secret
-  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  EMAIL_REGEX = /^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|‌​([A-Za-z0-9]+\++))*[‌​A-Za-z0-9]+@((\w+\-+‌​)|(\w+\.))*\w{1,63}\‌​.[a-zA-Z]{2,6}$/‌​i
   validates :name, :email, :password, presence: true, :on => :create
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
 
